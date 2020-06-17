46e83c8f7df979e3535485f723052ba19c460749
Ollie Bennett <olliebennett@gmail.com>
2020-06-18 00:14:31 +0100

    Improve links to W3W map

--- app/models/block.rb
+++ app/models/block.rb
@@ -3,4 +3,8 @@ class Block < ApplicationRecord
     box = RGeo::Cartesian::BoundingBox.create_from_points(southwest, northeast)
     RGeo::GeoJSON.encode(box.to_geometry).to_json
   end
+
+  def w3w_url
+    "https://what3words.com/#{w3w}"
+  end
 end

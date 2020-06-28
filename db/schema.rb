66992455bfa86131f530cdda32b20882f05aa778
Ollie Bennett <olliebennett@gmail.com>
2020-06-28 22:38:25 +0100

    Schema update for plots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_215303) do
+ActiveRecord::Schema.define(version: 2020_06_21_200544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_06_17_215303) do
     t.string "w3w", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "plot_id"
+    t.index ["plot_id"], name: "index_blocks_on_plot_id"
   end
 
+  create_table "plots", force: :cascade do |t|
+    t.string "title", null: false
+    t.geometry "polygon", limit: {:srid=>0, :type=>"st_polygon"}, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "blocks", "plots"
 end

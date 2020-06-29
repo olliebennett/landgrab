9f017097d8f40a1bd77bf080e84e3f19a2aad64e
Ollie Bennett <olliebennett@gmail.com>
2020-06-29 08:25:58 +0100

    Support dynamic 'center' of map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def homepage
+    if params[:center].present?
+      block = Block.find_by(w3w: params[:center])
+      @center = [block.midpoint.y, block.midpoint.x] if block
+    end
+
+    @center ||= [51.4778, -0.0014] # Greenwich Observatory
   end
 end

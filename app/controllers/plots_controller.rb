6620e7c4b4c3c5227d7216f724fdde2223827fe1
Ollie Bennett <olliebennett@gmail.com>
2020-07-02 00:35:40 +0100

    Enable drawing a pplot on the map to create

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,28 +1,20 @@
 class PlotsController < ApplicationController
   before_action :set_plot, only: [:show, :edit, :update, :destroy]
 
-  # GET /plots
-  # GET /plots.json
   def index
     @plots = Plot.all
   end
 
-  # GET /plots/1
-  # GET /plots/1.json
   def show
   end
 
-  # GET /plots/new
   def new
     @plot = Plot.new
   end
 
-  # GET /plots/1/edit
   def edit
   end
 
-  # POST /plots
-  # POST /plots.json
   def create
     @plot = Plot.new(plot_params)
 
@@ -37,8 +29,6 @@ class PlotsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /plots/1
-  # PATCH/PUT /plots/1.json
   def update
     respond_to do |format|
       if @plot.update(plot_params)
@@ -51,8 +41,6 @@ class PlotsController < ApplicationController
     end
   end
 
-  # DELETE /plots/1
-  # DELETE /plots/1.json
   def destroy
     @plot.destroy
     respond_to do |format|
@@ -62,13 +50,12 @@ class PlotsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_plot
-      @plot = Plot.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def plot_params
-      params.require(:plot).permit(:title, :polygon)
-    end
+  def set_plot
+    @plot = Plot.find(params[:id])
+  end
+
+  def plot_params
+    params.require(:plot).permit(:title, :polygon)
+  end
 end

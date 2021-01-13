515814cf074ed67e6e4510f42df065b76ec22794
Ollie Bennett <olliebennett@gmail.com>
2021-01-13 23:53:30 +0000

    Implement Bootstrap form/table styling

---
+++ app/helpers/bootstrap_helper.rb
@@ -0,0 +1,9 @@
+module BootstrapHelper
+  def active_navlink(title, href)
+    if current_page?(href)
+      link_to title, href, class: 'nav-link active', aria: { current: 'page' }
+    else
+      link_to title, href, class: 'nav-link'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :plots
-  resources :blocks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static_pages#homepage'
+
+  resources :plots, only: %i[index show new]
+  resources :blocks, only: %i[index show new]
 end

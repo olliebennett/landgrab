3c14efef608d4166cc5ccc0ff654da6bb78acb4f
Ollie Bennett <olliebennett@gmail.com>
2021-01-21 19:46:57 +0000

    Support editing plot title

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,5 +1,5 @@
 class PlotsController < ApplicationController
-  before_action :set_plot, only: [:show, :edit, :update, :destroy]
+  before_action :set_plot, only: %i[show edit update destroy]
 
   def index
     @plots = Plot.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#homepage'
 
-  resources :plots, only: %i[create index show new]
+  resources :plots, only: %i[create index show new edit update]
   resources :blocks, only: %i[create index show new]
 end

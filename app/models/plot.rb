4faebaa1bba290a5480248e7e21529fed7c8314a
Ollie Bennett <olliebennett@gmail.com>
2021-01-21 23:05:13 +0000

    Validate size of plot

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -4,6 +4,10 @@ class Plot < ApplicationRecord
   validates :title, presence: true
   validates :polygon, presence: true
 
+  validate :validate_bounding_box_dimensions
+
+  MAX_BOUNDING_BOX_DIMENSION = 0.005
+
   def centroid_coords
     RGeo::Cartesian::PointImpl.new(RGeo::Cartesian::Factory.new, polygon.centroid.x, polygon.centroid.y)
   end
@@ -16,5 +20,14 @@ class Plot < ApplicationRecord
     southwest = RGeo::Cartesian::PointImpl.new(RGeo::Cartesian::Factory.new, x_coords.min, y_coords.min)
 
     RGeo::Cartesian::BoundingBox.create_from_points(southwest, northeast)
+  end
+
+  def bounding_box_area
+    bounding_box.x_span * bounding_box.y_span
+  end
+
+  def validate_bounding_box_dimensions
+    errors.add(:polygon, 'is too wide') if bounding_box.x_span > MAX_BOUNDING_BOX_DIMENSION
+    errors.add(:polygon, 'is too high') if bounding_box.y_span > MAX_BOUNDING_BOX_DIMENSION
   end
 end

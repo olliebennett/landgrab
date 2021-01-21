c77e17a4d8ed543e21a41244c998b728e02460ba
Ollie Bennett <olliebennett@gmail.com>
2021-01-21 16:40:26 +0000

    Fix 'create' plot/block action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#homepage'
 
-  resources :plots, only: %i[index show new]
-  resources :blocks, only: %i[index show new]
+  resources :plots, only: %i[create index show new]
+  resources :blocks, only: %i[create index show new]
 end

2044caea2a1a47d7e5c0b698de976e17211e9e4b
Ollie Bennett <olliebennett@gmail.com>
2021-01-25 15:24:36 +0000

    RuboCop: Naming/MemoizedInstanceVariableName fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
     if params[:center].present?
       block = Block.find_by(w3w: params[:center])
       @center = [block.midpoint.y, block.midpoint.x] if block
+    else
+      @center = [51.4778, -0.0014] # Greenwich Observatory
     end
-
-    @center ||= [51.4778, -0.0014] # Greenwich Observatory
   end
 end

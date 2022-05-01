83a4eacc675ed279bbb98337ac9a4b886f7bac3c
Ollie Bennett <olliebennett@gmail.com>
2022-05-01 23:14:54 +0100

    Refactor stripe payload

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -12,29 +12,35 @@ class CheckoutController < ApplicationController
                 allow_other_host: true
   end
 
-  def success; end
+  def success
+    @block = Block.find_by_hashid!(params[:block])
+  end
 
   def cancel; end
 
   private
 
+  def stripe_checkout_payload
+    {
+      customer_email: current_user.email,
+      line_items: [{
+        # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: ENV.fetch('STRIPE_PRICE_ID_BLOCK'),
+        quantity: 1
+      }],
+      payment_intent_data: {
+        metadata: {
+          user: current_user.hashid,
+          blocks: @block.hashid
+        }
+      },
+      mode: 'payment',
+      success_url: checkout_success_url(block: @block.hashid),
+      cancel_url: checkout_cancel_url
+    }
+  end
+
   def create_stripe_checkout
-    @stripe_checkout = Stripe::Checkout::Session.create({
-                                                          customer_email: current_user.email,
-                                                          line_items: [{
-                                                            # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                                                            price: ENV.fetch('STRIPE_PRICE_ID_BLOCK'),
-                                                            quantity: 1
-                                                          }],
-                                                          payment_intent_data: {
-                                                            metadata: {
-                                                              user: current_user.hashid,
-                                                              blocks: @block.hashid
-                                                            }
-                                                          },
-                                                          mode: 'payment',
-                                                          success_url: checkout_success_url,
-                                                          cancel_url: checkout_cancel_url
-                                                        })
+    @stripe_checkout = Stripe::Checkout::Session.create(stripe_checkout_payload)
   end
 end

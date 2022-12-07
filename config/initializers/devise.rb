44c76676ffc1e96b716dc719d69dd53051f62e4a
Ollie Bennett <olliebennett@gmail.com>
2022-12-07 21:46:52 +0000

    Fallback for default FROM email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV.fetch('EMAIL_FROM_ADDRESS')
+  config.mailer_sender = ENV.fetch('EMAIL_FROM_ADDRESS', 'landgrab@example.com')
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

58cb5baef027495f10b30332e35a65e2a07f0164
Ollie Bennett <olliebennett@gmail.com>
2022-12-08 22:46:50 +0000

    Don't force login on claim landing page

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CheckoutController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[generate]
+  skip_before_action :authenticate_user!, only: %i[generate claim]
 
   # See docs/CHECKOUT.md
   def checkout

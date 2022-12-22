0cc7350b10d7a136c8a5fa2eda482fd495698ef7
Ollie Bennett <olliebennett@gmail.com>
2022-12-22 14:22:02 +0000

    Redirect user after registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,34 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :store_location
+
   protected
 
   def configure_permitted_parameters
     keys = %i[first_name last_name]
     devise_parameter_sanitizer.permit(:sign_up, keys:)
     devise_parameter_sanitizer.permit(:account_update, keys:)
+  end
+
+  # Store current URL for use in post-login/register redirect.
+  def store_location
+    return if user_signed_in?
+    return unless request.get?
+    return unless request.format.html? # skip JSON/CSV etc.
+    return if %w[
+      /
+      /users/sign_in
+      /users/sign_up
+      /users/password/new
+    ].include?(request.path)
+
+    store_location_for(:user, request.fullpath)
+  end
+
+  # Devise: Path for sending users to after they log in or register
+  def after_sign_in_path_for(_resource)
+    stored_location_for(:user) || root_path
   end
 
   private

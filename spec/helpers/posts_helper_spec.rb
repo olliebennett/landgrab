545c166d2c1ee8feb6c576d3214d79c46866a27a
Ollie Bennett <olliebennett@gmail.com>
2023-05-05 23:04:16 +0100

    Replace YouTube URLs with embed links instead

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe PostsHelper do
 
       expect(html).to eq %(<p><image src="/my_img.png" title="my_title" alt="my_alt_text" class="rounded img-fluid" /></p>\n)
     end
+
+    %w[
+      https://www.youtube.com/watch?v=abc-123
+    ].each do |yt_link|
+      it "renders iframe for YouTube URL #{yt_link}" do
+        html = helper.markdown(yt_link)
+
+        expect(html).to include %(<div class="ratio ratio-16x9">)
+        expect(html).to include 'src="https://www.youtube.com/embed/abc-123"'
+      end
+    end
   end
 end

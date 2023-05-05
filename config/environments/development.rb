4dd9f75e6d46b2559a5f2c92fc22c063fdd575c1
Ollie Bennett <olliebennett@gmail.com>
2023-05-05 20:57:17 +0100

    Allow hosting via ngrok for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow Ngrok tunnels
+  config.hosts << /.*\.ngrok\.io/
+  config.hosts << /.*\.ngrok-free\.app/
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?

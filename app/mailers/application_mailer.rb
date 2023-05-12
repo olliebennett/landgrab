1657a45b23e44b67fc32f05876511c8c7be894d2
Ollie Bennett <olliebennett@gmail.com>
2023-05-12 07:21:44 +0100

    Set email 'from' name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('EMAIL_FROM_ADDRESS', 'landgrab@example.com')
+  default from: "\"#{ENV.fetch('SITE_NAME', 'Landgrab')} Support\" <#{ENV.fetch('EMAIL_FROM_ADDRESS', 'landgrab@example.com')}>"
+
   layout 'mailer'
 end

37ba682aaeef63c1308f6549edb97ff05a9bd7a0
Ollie Bennett <olliebennett@gmail.com>
2023-06-15 23:09:18 +0100

    Display team page while logged out

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,8 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: %i[show]
 
+  skip_before_action :authenticate_user!, only: %i[show]
+
   def show
     log_event_mixpanel('Teams: Show', { authed: user_signed_in?, team: @team.slug })
   end

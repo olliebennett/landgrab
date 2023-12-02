a66464411a54e630202115da62283616bbe70576
Ollie Bennett <olliebennett@gmail.com>
2023-12-02 16:49:35 +0000

    Freeze FROM_EMAIL_WITH_NAME constant

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  FROM_EMAIL_WITH_NAME = "\"#{ENV.fetch('SITE_TITLE', 'Landgrab')} Support\" <#{ENV.fetch('EMAIL_FROM_ADDRESS', 'landgrab@example.com')}>"
+  FROM_EMAIL_WITH_NAME = "\"#{ENV.fetch('SITE_TITLE', 'Landgrab')} Support\" <#{ENV.fetch('EMAIL_FROM_ADDRESS', 'landgrab@example.com')}>".freeze
 
   default from: FROM_EMAIL_WITH_NAME
 

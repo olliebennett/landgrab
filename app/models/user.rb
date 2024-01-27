7f448d2d715a6b62550d9404ae0d747025ea668b
Ollie Bennett <olliebennett@gmail.com>
2024-01-27 06:24:53 +0000

    Auto-capitalise user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
                                  format: { with: /\Acus_[0-9a-zA-Z]+\z/ },
                                  uniqueness: true
 
+  before_create :titleize_lowercased_names
+
   def full_name
     [first_name, last_name].join(' ')
   end
@@ -30,5 +32,13 @@ class User < ApplicationRecord
     subscriptions.joins(:tile)
                  .where(tiles: { plot_id: plot.id })
                  .order(id: :desc).first # assume last = most likely to be active
+  end
+
+  private
+
+  def titleize_lowercased_names
+    # Replace each name's first letter (unless it already has a capital, as in 'de Santos')
+    self.first_name = first_name.sub(/\S/) { |s| s.mb_chars.upcase.to_s } if first_name == first_name.downcase
+    self.last_name = last_name.sub(/\S/) { |s| s.mb_chars.upcase.to_s } if last_name == last_name.downcase
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#titleize_lowercased_names' do
+    it 'titleizes entirely lowercased first and last names' do
+      simon = create(:user, first_name: 'simon', last_name: 'lópez')
+
+      expect(simon.first_name).to eq 'Simon'
+      expect(simon.last_name).to eq 'López'
+    end
+
+    it 'does not modify any already-capitalized first and last names' do
+      gertrude = create(:user, first_name: 'gerTrüde', last_name: 'van Dyk')
+
+      expect(gertrude.first_name).to eq 'gerTrüde'
+      expect(gertrude.last_name).to eq 'van Dyk'
+    end
+  end
 end

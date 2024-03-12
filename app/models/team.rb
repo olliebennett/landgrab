b2567ececf290a6c517ea3a40a52b03d6d210eb3
Ollie Bennett <olliebennett@gmail.com>
2024-03-12 06:24:27 +0000

    Simplify team creation with auto-slug

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,10 @@ class Team < ApplicationRecord
 
   validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  before_validation :normalize_slug
+
+  def normalize_slug
+    self.slug = slug.present? ? slug&.parameterize : title&.parameterize
+  end
 end
